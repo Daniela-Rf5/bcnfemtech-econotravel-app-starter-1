04f2d4811454b67b062f6f2eb6a93b01a02e3d0c
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-07-21 14:09:18 +0200

    agregando webpac ci¡onfiguracion de imagen

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -37,8 +37,22 @@ module.exports = {
                             ],
                         }
                     ]
+
                 }
             },
+            {
+                test:/\.(png|jpe?g|gif)$/i,
+                use:[
+                    {
+                        loader:'file-loader',
+                        options:{
+
+                        }
+
+                    },
+                ],
+            },
+
             {
                 test: /\.scss$/,
                 use: [
