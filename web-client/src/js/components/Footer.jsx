3097057c2cd478453778e66b242d87b2593f00eb
Marialauramn16 <hikco25@gmail.com>
2021-07-26 12:57:37 +0200

    agregando imagenes footer

---
+++ web-client/src/js/components/Footer.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export const Footer = () => (
+
+    <footer className={"footer"}>
+        <div>
+            Teléfono: 93 000 00 00
+            Correo: info@econotravel.com
+            Dirección: Carrer dels Viatgers, 28, 08000 Barcelona
+        </div>
+        <div>
+            <p>Copyright © 2021 Legacy Games-Todos los derechos reservados</p>
+        </div>
+    </footer>
+
+)
+
+
