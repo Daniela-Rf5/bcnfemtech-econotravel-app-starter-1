02299cb7cec5beee34a0e413a48fdb294d77788b
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-07-27 10:19:43 +0200

    imagenes de quienes somos y experiencias subidas, falta darle estilo

--- web-client/src/js/components/App.jsx
+++ web-client/src/js/components/App.jsx
@@ -7,8 +7,11 @@ import {FormularioReserva} from "./Reservas";
 import {FinalReserva} from "./FinalReserva";
 import {Footer} from "./Footer";
 import bicicleta from "./assets/bicicleta.jpg";
-
-
+import viajeBarco from "./assets/viajeBarco.jpg";
+import barcelonaModerna from "./assets/barcelonaModerna.jpg";
+import huerto from "./assets/huerto.jpg";
+import montañaSagrada from "./assets/montañaSagrada.jpg";
+import fundadora from "./assets/fundadora.jpg";
 export class App extends React.Component {
     render() {
         const experiences = [
@@ -22,12 +25,14 @@ export class App extends React.Component {
 
             {
                 id: 2,
+                image: <img src={viajeBarco}/>,
                 name: 'Descubre la costa en barco de vela',
                 description: 'Disfruta de un hermoso paseo acuático en barco de vela por la increíble costa de Barcelona. Una escapada veraniega apta incluso para los días más calurosos del año. Descubre los encantadores alrededores de la ciudad de Barcelona y visita desde el mar sus más impresionantes playas y calas. Comenzaremos la excursión en el Puerto de Barcelona, desde donde partiremos hacia el norte para visitar playas como la Mar Bella, la Playa de la Mora y la Playa de los Pescadores. A bordo de la embarcación podremos disfrutar de una selección de quesos y embutidos catalanes acompañada de cava brut y zumos de frutas. Asimismo, pararemos cerca de la Playa de Montgat para realizar una actividad de buceo de superficie que nos permitirá apreciar la diversidad de la fauna marítima local y su ecosistema. Finalizaremos la excursión en el mismo puerto del que partimos.',
                 pricePerPerson: 280
             },
             {
                 id: 3,
+                image: <img src={barcelonaModerna}/>,
                 name: 'Descubre la Barcelona Modernista de noche',
                 description: 'Desplazarse a pie es una de las mejores formas de descubrir las maravillas modernistas que se esconden en el barcelonés distrito del Eixample, ubicado en el centro de la ciudad. En esta excursión de cuatro horas, descubriremos los principales emblemas del modernismo y visitaremos los templos y edificios más célebres del arquitecto Gaudí. El tour incluye visita guiada al interior de la Casa Batlló y la Sagrada Familia, así como parada para cenar en el restaurante típico catalán Can Masiá, donde disfrutaremos de las mejores carnes de la región acompañadas de vinos de las tierras del Baix Empordá. El restaurante también ofrece opciones vegetarianas y veganas así como menú para niños. Cava aparte.',
                 pricePerPerson: 200
@@ -36,11 +41,13 @@ export class App extends React.Component {
             {
                 id: 4,
                 name: 'Del huerto a la mesa',
+                image: <img src={huerto}/>,
                 description: 'Comparte con los más pequeños las maravillas de la naturaleza. En esta excursión breve de tres horas, podréis plantar vuestras propias hortalizas y verduras en el huerto de Can Gilabert, ubicado en el corazón del Montbaig. Durante una hora y media, aprenderéis a cuidar de vuestro propio huerto, plantaréis y abonaréis vuestras propias plantas, y también recogeréis parte de los ingredientes que los expertos chefs de Can Gilabert utilizarán para preparar la posterior cena, donde disfrutaréis de la experiencia de consumir productos recién recogidos de la tierra con vuestras propias manos. Actividad ideal para familiarizar a los más pequeños de la familia con el estilo de vida rural y la agricultura sostenible. Asimismo, obtendréis como obsequio una botella de aceite virgen extra cultivado y preparado en Can Gilabert, obra de la familia Gilabert, que lleva más de 80 años cultivando olivos.',
                 pricePerPerson: 145
             },
             {
                 id: 5,
+                image: <img src={montañaSagrada}/>,
                 name: 'Arte en la montaña sagrada',
                 description: 'Disfruta de la impresionante Montaña de Montserrat donde encontraréis el Museo con las salas modernistas de Puig i Cadafalch, lugar que acoge colecciones de gran valor en el corazón de la emblemática montaña. En el Museo encontraréis pinturas del Renacimiento y del Barroco que conviven con autores de los siglos XIX y XX, objetos del Próximo Oriente y orfebrería. Además de disfrutar de un paseo guiado por la montaña y el Museo, esta actividad incluye tentempié variado acompañado de vino espumoso, cava brut o refresco.',
                 pricePerPerson: 125
@@ -53,6 +60,7 @@ export class App extends React.Component {
             <Switch>
                 <Route path="/about">
                     <About/>
+                    image: <img src={fundadora}/>
                 </Route>
                 <Route path="/ExperiencePanel">
                     <ExperiencePanel experiences={experiences}/>
