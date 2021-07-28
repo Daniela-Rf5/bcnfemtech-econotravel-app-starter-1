d0ea3b5bdc5809f2a4749f9095adadaafafba631
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-07-28 10:06:30 +0200

    agragando foto fundadora

--- web-client/src/js/components/about.jsx
+++ web-client/src/js/components/about.jsx
@@ -1,23 +1,33 @@
 import * as React from "react";
-
-
-export const About = () => <section className={"parrafos"}>
-
-    <p>
-        Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina Herrán, realizó en bicicleta por
-        Cataluña en el verano de 2001. Durante ese periodo, descubrió que las opciones turísticas que ofrecían ciudades
-        como Barcelona, Girona y Reus dependían de transportes contaminantes y actividades poco respetuosas con la
-        naturaleza y el entorno, algo que causaba un progresivo deterioro de los increíbles parajes naturales de
-        Cataluña.
-
-    </p>
-
-    <p>
-        Preocupada por la falta de alternativas eco-friendly, Marina se propuso fundar una agencia de experiencias
-        turísticas donde el respeto por la naturaleza fuera la principal prioridad. Así nació Econotravel Barcelona.
-    </p>
-    <p>Teléfono: 93 000 00 00</p>
-    <p>Correo: info@econotravel.com</p>
-    <p>Dirección: Carrer dels Viatgers, 28, 08000 Barcelona</p>
-
-</section>
+import fundadora from "./assets/fundadora.jpg";
+export const About = () => {
+    return (
+        <section>
+            <img className={"imgFundadora"} src={fundadora}/>
+            <div className={"parrafos"}>
+                <p>
+                    Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina Herrán, realizó en
+                    bicicleta por
+                    Cataluña en el verano de 2001. Durante ese periodo, descubrió que las opciones turísticas que
+                    ofrecían ciudades
+                    como Barcelona, Girona y Reus dependían de transportes contaminantes y actividades poco respetuosas
+                    con la
+                    naturaleza y el entorno, algo que causaba un progresivo deterioro de los increíbles parajes
+                    naturales de
+                    Cataluña.
+                </p>
+                <p>
+                    Preocupada por la falta de alternativas eco-friendly, Marina se propuso fundar una agencia de
+                    experiencias
+                    turísticas donde el respeto por la naturaleza fuera la principal prioridad. Así nació Econotravel
+                    Barcelona.
+                </p>
+            </div>
+            <div>
+                <p>Teléfono: 93 000 00 00</p>
+                <p>Correo: info@econotravel.com</p>
+                <p>Dirección: Carrer dels Viatgers, 28, 08000 Barcelona</p>
+            </div>
+        </section>
+    )
+}
