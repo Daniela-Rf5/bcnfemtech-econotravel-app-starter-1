e98de7275c0c2cdcda92264b1435b056531f0847
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-07-29 14:57:59 +0200

    agregando estilo en pagina reservado

--- web-client/src/js/components/FinalReserva.jsx
+++ web-client/src/js/components/FinalReserva.jsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 import {FormularioReserva} from "./Reservas";
+import reservar from "./assets/reservar.jpg";
+
+
 
 export const FinalReserva = () => <section>
-
-    <h1 className={"reservado"}>¡Gracias por realizar tu reserva con nosotros!</h1>
-
+    <div className={"contenedor"}>
+        <img className={"imgInicio"} src={reservar}/>
+        <div className={"textInicio"}>
+            <h1>¡Gracias por realizar tu reserva con nosotros!</h1>
+        </div>
+    </div>
 </section>
+
